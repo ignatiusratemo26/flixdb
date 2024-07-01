377a379526fc96b10bc4814de5733e59ad90f715
ignatiusratemo26 <ignatiusratemo5@gmail.com>
2024-07-01 21:37:30 +0300

    Fixed serializer

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from watchlist.models import Movie
 
-@api_view
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
     name = serializers.CharField()
     desc = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.desc = validated_data.get('desc', instance.desc)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 from watchlist.models import Movie
-from django.http import JsonResponse
 from watchlist.api.serializers import MovieSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)  # Correct usage of many=True
+        return Response(serializer.data)
+    if request.method == 'POST':
+        # Serialize the request
+        serializer = MovieSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
 
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk = pk)
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=404)
+    if request.method == 'PUT':
+        """ PUT -> updating every field. But patch is updating one field """
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204)
+
+    
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
